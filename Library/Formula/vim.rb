9a1aed219b6d04111f136cc662c0c7defe473c1f
Yoshimasa Niwa <niw@niw.at>
2011-03-30 20:36:38 -0700

    Bump vim version to 7.3.146

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -6,8 +6,8 @@ require 'formula'
 class Vim <Formula
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :revision => '26fb122355d4d6b04850f7608f1d82da8dbc51c7'
-  version '7.3.107'
+  url 'https://vim.googlecode.com/hg/', :revision => '24b41d74f6a9e00514dcf1c3c63683a0b6ff2991'
+  version '7.3.146'
   homepage 'http://www.vim.org/'
 
   head 'https://vim.googlecode.com/hg/'
